8d0e5d4108f28678cb8c58f4a8a10716799f5d06
Hirota,Shinnosuke S <s.hirota@student.fontys.nl>
2018-12-17 15:47:35 +0100

    add user export to excel sheet

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\User;
+use App\Exports\UsersExport;
+use Maatwebsite\Excel\Facades\Excel;
 
 class UserController extends Controller
 {
@@ -13,8 +15,13 @@ class UserController extends Controller
         $users = User::all();
         return view('users.user-list',['users'=>$users]);
     }
-    public function show($id){
+    public function show($id)
+    {
         $selected_user = User::find($id);
         return view('users.user-detail')->with('selected_user', $selected_user);
+    }
+    public function export()
+    {
+        return Excel::download(new UsersExport, 'users.xlsx');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -29,4 +29,5 @@ Route::resource('posts', 'PostsController');
 Route::get('/dashboard', 'DashboardController@index');
 
 Route::get('/users', 'UserController@index');
+Route::get('users/excel-export', 'UserController@export');
 Route::get('/users/{id}', 'UserController@show');
