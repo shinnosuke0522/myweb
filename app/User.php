325e56e2b6e47700e066edae080f2ca542ce3bc8
Hirota,Shinnosuke S <s.hirota@student.fontys.nl>
2018-12-18 14:03:23 +0100

    add general users api

--- app/User.php
+++ app/User.php
@@ -31,4 +31,8 @@ class User extends Authenticatable
     public function posts(){
         return $this->hasMany('App\Post');
     }
+    public function getId()
+    {
+        return $this->id;
+    }
 }
